c0496e3dffee34850742d9a0385d409c0a97ea78
Nacelio M Barbosa <naceliombdev@gmail.com>
2022-05-09 01:21:13 -0300

    add route for card

--- src/api/components/colaborator/controllers.js
+++ src/api/components/colaborator/controllers.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const Colaborator = require("./model");
 const {getDoc} = require("../../services/spreadSheet");
+const path = require("path");
+const ejs = require("ejs");
+const { log } = require("console");
 
 const router = express.Router();
 
 router.get("/colaborators/:doc", async (req, res) =>{
-    console.log("aquela");
+    console.log("access - http://localhost:3000/api/v1/colaborators/:doc");
     const docId = req.params.doc;
     let colaborators = [];
     if(!docId){
@@ -38,8 +41,11 @@ router.get("/colaborators/:doc", async (req, res) =>{
 });
 
 router.get("/colaborators/email-enterprise/:doc", async(req,res)=>{
+    console.log("access - http://localhost:3000/colaborators/email-enterprise/:doc");
     const docId = req.params.doc;
     const empty = req.query.empty;
+    const date = req.query.date;
+
     const template = req.query.template;
     let colaborators = [];
     if(!docId){
@@ -53,11 +59,17 @@ router.get("/colaborators/email-enterprise/:doc", async(req,res)=>{
 
             sheet.getRows().then(rows => {
                 rows.map( row => {
-                    if(empty == "true"){
+                    if(empty == "true" && !date ){
                         if(!row['E-MAIL INSTITUCIONAL']&&row['NOME']){
                             const colaborator = new Colaborator(row['NOME'], row['SETOR'], row['ESCALA'], row['FEIRISTA'] == "TRUE" ? true : false, row['CONTATO'], row['STATUS'], row['FUNÇÃO'], row['CPF'], row['E-MAIL'], row['E-MAIL INSTITUCIONAL'], new Date(row['DATA - ADMISSÃO']), row['SITUAÇÃO - DOCUMENTOS'], row['OBSERVAÇÕES']);
                             colaborators.push(colaborator);
                         }
+                    }else if(empty == "true" && date){
+                        if(!row['E-MAIL INSTITUCIONAL']&&row['NOME']&& row['TEMPORARIO'] != "TRUE" && row['DATA - ADMISSÃO'] == date){
+                            const colaborator = new Colaborator(row['NOME'], row['SETOR'], row['ESCALA'], row['FEIRISTA'] == "TRUE" ? true : false, row['CONTATO'], row['STATUS'], row['FUNÇÃO'], row['CPF'], row['E-MAIL'], row['E-MAIL INSTITUCIONAL'], new Date(row['DATA - ADMISSÃO']), row['SITUAÇÃO - DOCUMENTOS'], row['OBSERVAÇÕES']);
+                            colaborators.push(colaborator);
+                        }
+
                     }else{
                         if(row['E-MAIL INSTITUCIONAL']&&row['NOME']){
                             const colaborator = new Colaborator(row['NOME'], row['SETOR'], row['ESCALA'], row['FEIRISTA'] == "TRUE" ? true : false, row['CONTATO'], row['STATUS'], row['FUNÇÃO'], row['CPF'], row['E-MAIL'], row['E-MAIL INSTITUCIONAL'], new Date(row['DATA - ADMISSÃO']), row['SITUAÇÃO - DOCUMENTOS'], row['OBSERVAÇÕES']);
@@ -87,6 +99,7 @@ router.get("/colaborators/email-enterprise/:doc", async(req,res)=>{
 });
 
 router.get("/colaborators/:name/:doc", async (req,res) => {
+    console.log("access - http://localhost:3000/colaborators/:name/:doc");
     const docId = req.params.doc;
     const name = req.params.name
     let colaborators = [];
@@ -129,6 +142,67 @@ router.get("/colaborators/:name/:doc", async (req,res) => {
         console.log(err);
         res.status(500).json("SERVER ERROR");
     }
+});
+
+router.get("/archives/card/:name/:doc", async(req,res) => {
+    console.log("access - http://localhost:3000/archives/card");
+
+    const docId = req.params.doc;
+    const name = req.params.name
+    let colaborators = [];
+
+    const filePath = path.join(__dirname, "../", "../", "global", "welcome.ejs");
+
+    if(!docId){
+        res.status(400).json("INVALID DOC ID");
+        return;
+    }
+
+    try{
+        getDoc(docId).then( async (doc) =>{
+            const sheet = doc.sheetsByIndex[0];
+            console.log(`SHEET NAME: ${sheet.title}`);
+
+            sheet.getRows().then( rows => {
+                if(name){
+                    rows.map( row => {
+                        if(row['NOME'].toUpperCase() == name.toUpperCase()){
+                            const colaborator = new Colaborator(row['NOME'], row['SETOR'], row['ESCALA'], row['FEIRISTA'] == "TRUE" ? true : false, row['CONTATO'], row['STATUS'], row['FUNÇÃO'], row['CPF'], row['E-MAIL'], row['E-MAIL INSTITUCIONAL'], new Date(row['DATA - ADMISSÃO']), row['SITUAÇÃO - DOCUMENTOS'], row['OBSERVAÇÕES']);
+                            colaborators.push(colaborator);
+                            
+                        }
+                    });
+                    if(!colaborators.length){
+                        res.status(404).json("COLABORATOR NOT FOUND");
+                        return;
+                    }
+                    ejs.renderFile(filePath, { colaborators:colaborators }, (err, data) => {
+                        if(err){
+                            console.log(err);
+                            return res.status(500).json("Erro na leitura do arquivo");
+                        }
+
+                        return res.send(data);
+                    });
+                    // res.status(200).json(colaborators);
+                    // return;
+                }else{
+                    res.status(400).json("INVALID NAME");
+                    return;
+                }
+                
+
+            });
+        });
+    }catch(err){
+        console.log(err);
+        res.status(500).json("SERVER ERROR");
+    }
+
+
+
+   
+
 });
 
 module.exports = router;
