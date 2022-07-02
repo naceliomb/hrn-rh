0723863dd0f2f0f56061928d4b8be6a7fb0e6957
Nacelio M Barbosa <naceliombdev@gmail.com>
2022-07-02 11:04:56 -0300

    test deploy in heroku

--- src/api/middleware/generatePDF.js
+++ src/api/middleware/generatePDF.js
@@ -1,10 +1,9 @@
 const puppeteer = require('puppeteer');
 
 const generatePDF = async (content) => {
-    console.log(content);
+    // console.log(content);
     const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
-    // await page.goto('https://google.com', {waitUntil: 'networkidle0'});
     await page.setContent(content);
     const pdf = await page.pdf({ format: 'A4' });
     await page.close();
