46044812eaba69fe86c61f22788ef9671f5bc3fb
Nacelio M Barbosa <naceliombdev@gmail.com>
2022-07-02 15:01:48 -0300

    trying puppeteer

--- src/api/components/cards/controllers.js
+++ src/api/components/cards/controllers.js
@@ -104,100 +104,174 @@ router.get('/archives/cards/:doc', async (req, res) => {
     }
 
     try {
-        getDoc(docId)
-            .then(async (doc) => {
-                const sheet = doc.sheetsByIndex[0];
-                console.log(`SHEET NAME: ${sheet.title}`);
+        getDoc(docId).then(async (doc) => {
+            const sheet = doc.sheetsByIndex[0];
+            console.log(`SHEET NAME: ${sheet.title}`);
 
-                sheet.getRows().then(async (rows) => {
-                    if (date) {
-                        rows.map((row) => {
-                            if (row['DATA - ADMISSÃO'] == date) {
-                                const day = row['DATA - ADMISSÃO'].substring(0, 2);
-                                const month = row['DATA - ADMISSÃO'].substring(3, 5);
-                                const year = row['DATA - ADMISSÃO'].substring(row['DATA - ADMISSÃO'].length - 4);
+            sheet.getRows().then(async (rows) => {
+                if (date) {
+                    rows.map((row) => {
+                        if (row['DATA - ADMISSÃO'] == date) {
+                            const day = row['DATA - ADMISSÃO'].substring(0, 2);
+                            const month = row['DATA - ADMISSÃO'].substring(3, 5);
+                            const year = row['DATA - ADMISSÃO'].substring(row['DATA - ADMISSÃO'].length - 4);
 
-                                const date = new Date(year + '-' + month + '-' + day);
-                                const colaborator = new Colaborator(
-                                    row['NOME'],
-                                    row['SETOR'],
-                                    row['ESCALA'],
-                                    row['FEIRISTA'] == 'TRUE' ? true : false,
-                                    row['TEMPORARIO'] == 'TRUE' ? true : false,
-                                    row['CONTATO'],
-                                    row['STATUS'],
-                                    row['FUNÇÃO'],
-                                    row['CPF'],
-                                    row['E-MAIL'],
-                                    row['E-MAIL INSTITUCIONAL'],
-                                    date,
-                                    row['SITUAÇÃO - DOCUMENTOS'],
-                                    row['OBSERVAÇÕES']
-                                );
-                                colaborators.push(colaborator);
-                            }
-                        });
-                        if (!colaborators.length) {
-                            res.status(404).json('COLABORATORS NOT FOUND');
-                            return;
+                            const date = new Date(year + '-' + month + '-' + day);
+                            const colaborator = new Colaborator(
+                                row['NOME'],
+                                row['SETOR'],
+                                row['ESCALA'],
+                                row['FEIRISTA'] == 'TRUE' ? true : false,
+                                row['TEMPORARIO'] == 'TRUE' ? true : false,
+                                row['CONTATO'],
+                                row['STATUS'],
+                                row['FUNÇÃO'],
+                                row['CPF'],
+                                row['E-MAIL'],
+                                row['E-MAIL INSTITUCIONAL'],
+                                date,
+                                row['SITUAÇÃO - DOCUMENTOS'],
+                                row['OBSERVAÇÕES']
+                            );
+                            colaborators.push(colaborator);
                         }
-                        if (!fs.existsSync(downloadsPath)) {
-                            fs.mkdirSync(downloadsPath);
-                        }
-                        if (!fs.existsSync(dateDirectoryPath)) {
-                            fs.mkdirSync(dateDirectoryPath);
-                        }
-                        colaborators.forEach(async (colaborator, index) => {
-                            console.log(`COLABORATOR: ${colaborator.name}`);
-                            const template = fs.readFileSync(filePath, 'utf-8');
-                            const html = ejs.render(template, { colaborator: colaborator });
-                            if(format == html){
-                                console.log('FORMAT: HTML');
-                                fs.writeFileSync(path.join(dateDirectoryPath, `${colaborator.name}.html`), html, 'utf-8');
-                            }else{
-                                console.log('FORMAT: PDF');
-                                const pdf = await generatePDF(html);
-                                fs.writeFileSync(path.join(dateDirectoryPath, `${colaborator.name}.pdf`), pdf, 'binary');
-                            }
-                        });
-
-                        return res.status(201).json({message: 'Created sucessfully', status: 201});
-                    } else {
-                        res.status(504).json({message: 'Enter date', status: 504});
+                    });
+                    if (!colaborators.length) {
+                        res.status(404).json('COLABORATORS NOT FOUND');
                         return;
                     }
-                });
+                    if (!fs.existsSync(downloadsPath)) {
+                        fs.mkdirSync(downloadsPath);
+                    }
+                    if (!fs.existsSync(dateDirectoryPath)) {
+                        fs.mkdirSync(dateDirectoryPath);
+                    }
+                    colaborators.forEach(async (colaborator, index) => {
+                        console.log(`COLABORATOR: ${colaborator.name}`);
+                        const template = fs.readFileSync(filePath, 'utf-8');
+                        const html = ejs.render(template, { colaborator: colaborator });
+                        if (format == html) {
+                            console.log('FORMAT: HTML');
+                            fs.writeFileSync(path.join(dateDirectoryPath, `${colaborator.name}.html`), html, 'utf-8');
+                        } else {
+                            console.log('FORMAT: PDF');
+                            const pdf = await generatePDF(html);
+                            fs.writeFileSync(path.join(dateDirectoryPath, `${colaborator.name}.pdf`), pdf, 'binary');
+                        }
+                    });
+
+                    return res.status(201).json({ message: 'Created sucessfully', status: 201 });
+                } else {
+                    res.status(504).json({ message: 'Enter date', status: 504 });
+                    return;
+                }
             });
+        });
     } catch (err) {
         console.log(err);
         res.status(500).json('SERVER ERROR');
     }
 });
 
-router.get('/archives/download', async (req, res) =>{
+router.get('/archives/heroku', async (req, res) => {
+    let colaborators = [
+        {
+            name: 'Nacelio Moreira Barbosa',
+            departament: 'SCIH',
+            workSchedule: 'Comercial',
+            diarist: true,
+            temp: false,
+            contact: '88999999999',
+            status: 'OK',
+            role: 'Auxiliar Administrativo',
+            cpf: '000.000.000-00',
+            email: 'naceliombdev@gmail.com',
+            emailEnterprise: 'naceliombdev@gmail.com',
+            admissionDate: '12/07/2022',
+            documentStatus: 'OK',
+            comments: 'Encaminhar Link Integração',
+        },
+        {
+            name: 'Nacelio Barbosa',
+            departament: 'SCIH',
+            workSchedule: 'Comercial',
+            diarist: true,
+            temp: false,
+            contact: '88999999999',
+            status: 'OK',
+            role: 'Auxiliar Administrativo',
+            cpf: '000.000.000-00',
+            email: 'naceliombdev@gmail.com',
+            emailEnterprise: 'naceliombdev@gmail.com',
+            admissionDate: '12/07/2022',
+            documentStatus: 'OK',
+            comments: 'Encaminhar Link Integração',
+        },
+        {
+            name: 'Nacelio Moreira',
+            departament: 'SCIH',
+            workSchedule: 'Comercial',
+            diarist: true,
+            temp: false,
+            contact: '88999999999',
+            status: 'OK',
+            role: 'Auxiliar Administrativo',
+            cpf: '000.000.000-00',
+            email: 'naceliombdev@gmail.com',
+            emailEnterprise: 'naceliombdev@gmail.com',
+            admissionDate: '12/07/2022',
+            documentStatus: 'OK',
+            comments: 'Encaminhar Link Integração',
+        },
+    ];
+
+    const filePath = path.join(__dirname, '../', '../', 'global', 'welcomeAll.ejs');
+    const downloadsPath = path.join(__dirname, '../', '../', 'public', 'downloads');
+
+    if (!fs.existsSync(downloadsPath)) {
+        fs.mkdirSync(downloadsPath);
+    }
+
+    try{
+        colaborators.forEach(async (colaborator) => {
+            console.log(`COLABORATOR: ${colaborator.name}`);
+            const template = fs.readFileSync(filePath, 'utf-8');
+            const html = ejs.render(template, { colaborator: colaborator });
+    
+            const pdf = await generatePDF(html);
+            fs.writeFileSync(path.join(downloadsPath, `${colaborator.name}.pdf`), pdf, 'binary');
+        });
+    
+        return res.status(201).json({message: 'created'})
+    }catch(e){
+        console.log(e);
+        return res.status(503).json({message: 'Server Error'});
+    }
+    
+});
+
+router.get('/archives/download', async (req, res) => {
     console.log('access - http://localhost:3000/archives/download');
     const date = req.query.date;
-    
-    if(!date){
-        return res.status(504).json({message: 'Enter with valid date', status: 504});
+
+    if (!date) {
+        return res.status(504).json({ message: 'Enter with valid date', status: 504 });
     }
 
     const downloadsPath = path.join(__dirname, '../', '../', 'public', 'downloads');
     const dateDirectoryPath = path.join(downloadsPath, date.replace(/[/]/g, '-'));
 
     if (!fs.existsSync(dateDirectoryPath)) {
-        return res.status(404).json({message: 'Files not found', status: 404});
+        return res.status(404).json({ message: 'Files not found', status: 404 });
     }
 
     const files = await listFiles(dateDirectoryPath);
     const merged = await mergePDF(files, dateDirectoryPath);
     const pdfMerged = fs.readFileSync(merged);
     res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdfMerged.length });
-    res.set("Content-Disposition", `attachment;filename=merged_${date.replace(/[/]/g, '-')}.pdf`);
-    res.set("Content-Type", "application/octet-stream");
+    res.set('Content-Disposition', `attachment;filename=merged_${date.replace(/[/]/g, '-')}.pdf`);
+    res.set('Content-Type', 'application/octet-stream');
     return res.status(200).send(pdfMerged);
-
-
 });
 
 router.get('/cleaner', async (req, res) => {
@@ -205,13 +279,13 @@ router.get('/cleaner', async (req, res) => {
         const downloadsPath = path.join(__dirname, '../', '../', 'public', 'downloads');
         if (fs.existsSync(downloadsPath)) {
             fs.rmSync(downloadsPath, { recursive: true, force: true });
-            return res.status(200).json({message: 'Removed all files', status: 200});
+            return res.status(200).json({ message: 'Removed all files', status: 200 });
         }
 
-        return res.status(404).json({message: 'Files not found', status: 404});
+        return res.status(404).json({ message: 'Files not found', status: 404 });
     } catch (err) {
         console.log(err);
-        return res.status(500).json({message: 'Internal server error', status: 500});
+        return res.status(500).json({ message: 'Internal server error', status: 500 });
     }
 });
 
